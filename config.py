fcda0a8961d9a5a6775c78e38464a0bd32b1f438
wzhbaba <1595401605@qq.com>
2025-09-03 13:57:13 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,30 @@
+from legged_gym import LEGGED_GYM_ROOT_DIR
+import numpy as np
+import yaml
+
+
+class Config:
+    def __init__(self, file_path) -> None:
+        with open(file_path, "r") as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+
+            self.lowcmd_topic = config["lowcmd_topic"]
+            self.lowstate_topic = config["lowstate_topic"]
+
+            self.policy_path = config["policy_path"].replace("{LEGGED_GYM_ROOT_DIR}", LEGGED_GYM_ROOT_DIR)
+            self.encoder_path = config["encoder_path"].replace("{LEGGED_GYM_ROOT_DIR}", LEGGED_GYM_ROOT_DIR)
+
+            self.leg_joint2motor_idx = config["leg_joint2motor_idx"]
+            self.kps = config["kps"]
+            self.kds = config["kds"]
+            self.default_angles = np.array(config["default_angles"], dtype=np.float32)
+
+            self.ang_vel_scale = config["ang_vel_scale"]
+            self.dof_pos_scale = config["dof_pos_scale"]
+            self.dof_vel_scale = config["dof_vel_scale"]
+            self.action_scale = config["action_scale"]
+            self.cmd_scale = np.array(config["cmd_scale"], dtype=np.float32)
+            self.max_cmd = np.array(config["max_cmd"], dtype=np.float32)
+
+            self.num_actions = config["num_actions"]
+            self.num_obs = config["num_obs"]
---
+++ deploy_real.py
@@ -0,0 +1,164 @@
+from legged_gym import LEGGED_GYM_ROOT_DIR
+import torch
+import time
+import sys
+import numpy as np
+
+from unitree_sdk2py.core.channel import ChannelPublisher, ChannelSubscriber
+from unitree_sdk2py.core.channel import ChannelFactoryInitialize
+from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowCmd_, unitree_go_msg_dds__LowState_
+from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowCmd_, LowState_
+from unitree_sdk2py.utils.thread import RecurrentThread
+from unitree_sdk2py.utils.crc import CRC
+
+from common.remote_controller import KeyMap, RemoteController
+from common.rotation_helper import get_gravity_orientation
+from config import Config
+
+
+class RLDeploy:
+
+    def __init__(self, config: Config) -> None:
+
+        self.config = config
+        self.num_actions = self.config.num_actions
+
+        self.remote_controller = RemoteController()
+
+        # Initialize the policy network
+        self.policy = torch.jit.load(config.policy_path)
+        self.encoder = torch.jit.load(config.encoder_path)
+        # Initializing process variables
+        self.qj = np.zeros(config.num_actions, dtype=np.float32)
+        self.dqj = np.zeros(config.num_actions, dtype=np.float32)
+        self.action = np.zeros(config.num_actions, dtype=np.float32)
+        self.obs_history = np.zeros(config.num_obs * 5, dtype=np.float32)
+        self.target_dof_pos = config.default_angles.copy()
+        self.obs = np.zeros(config.num_obs, dtype=np.float32)
+        self.cmd = np.zeros_like(config.max_cmd, dtype=np.float32)
+
+        self.low_cmd = unitree_go_msg_dds__LowCmd_()
+        self.low_state = unitree_go_msg_dds__LowState_()
+
+        self.lowcmd_publisher_ = ChannelPublisher("rt/lowcmd", LowCmd_)
+        self.lowcmd_publisher_.Init()
+
+        self.lowstate_subscriber = ChannelSubscriber("rt/lowstate", LowState_)
+        self.lowstate_subscriber.Init(self.LowStateHandler, 10)
+
+        self.move = 0
+        self.stop = 1
+        self.ready = 0
+
+        self.InitLowCmd()
+        self.InitRecurrentThread()
+
+    # Handler for low state messages
+    def LowStateHandler(self, msg: LowState_):
+        self.low_state = msg
+        self.remote_controller.set(self.low_state.wireless_remote)
+
+    # Initialize recurrent threads
+    def InitRecurrentThread(self):
+        self.lowCmdWriteThreadPtr = RecurrentThread(interval=0.001, target=self.SendCmd, name="writebasiccmd")
+        self.lowCtrlThreadPtr = RecurrentThread(interval=0.02, target=self.Ctrl, name="lowCtrlThread")
+        self.FSMThreadPtr = RecurrentThread(interval=0.01, target=self.FSM, name="FSMThread")
+        self.lowCmdWriteThreadPtr.Start()
+        self.lowCtrlThreadPtr.Start()
+        self.FSMThreadPtr.Start()
+
+    # Initialize low command message
+    def InitLowCmd(self):
+        self.low_cmd.head[0] = 0xFE
+        self.low_cmd.head[1] = 0xEF
+        self.low_cmd.level_flag = 0xFF
+        self.low_cmd.gpio = 0
+        self.PosStopF = 2.146e9
+        self.VelStopF = 16000.0
+
+        for i in range(20):
+            self.low_cmd.motor_cmd[i].mode = 0x01  # (PMSM) mode
+            self.low_cmd.motor_cmd[i].q = self.PosStopF
+            self.low_cmd.motor_cmd[i].kp = 0
+            self.low_cmd.motor_cmd[i].dq = self.VelStopF
+            self.low_cmd.motor_cmd[i].kd = 0
+            self.low_cmd.motor_cmd[i].tau = 0
+
+    def CreateDampingCmd(self, cmd: LowCmd_):
+        size = len(cmd.motor_cmd)
+        for i in range(size):
+            cmd.motor_cmd[i].q = 0
+            cmd.motor_cmd[i].qd = 0
+            cmd.motor_cmd[i].kp = 0
+            cmd.motor_cmd[i].kd = 8
+            cmd.motor_cmd[i].tau = 0
+
+    def Forward(self):
+        # Get the current joint position and velocity
+        for i in range(len(self.config.leg_joint2motor_idx)):
+            motor_idx = self.config.leg_joint2motor_idx[i]
+            self.qj[i] = self.low_state.motor_state[motor_idx].q
+            self.dqj[i] = self.low_state.motor_state[motor_idx].dq
+
+        # imu_state quaternion: w, x, y, z
+        quat = self.low_state.imu_state.quaternion
+        ang_vel = np.array([self.low_state.imu_state.gyroscope], dtype=np.float32)
+
+        gravity_orientation = get_gravity_orientation(quat)
+        qj_obs = self.qj.copy()
+        dqj_obs = self.dqj.copy()
+        qj_obs = (qj_obs - self.config.default_angles) * self.config.dof_pos_scale
+        dqj_obs = dqj_obs * self.config.dof_vel_scale
+        ang_vel = ang_vel * self.config.ang_vel_scale
+
+        self.cmd[0] = self.remote_controller.ly
+        self.cmd[1] = self.remote_controller.lx * -1
+        self.cmd[2] = self.remote_controller.rx * -1
+
+        self.obs[:3] = ang_vel
+        self.obs[3:6] = gravity_orientation
+        self.obs[6:6 + self.num_actions] = qj_obs
+        self.obs[6 + self.num_actions:6 + 2 * self.num_actions] = dqj_obs
+        self.obs[6 + 2 * self.num_actions:6 + 3 * self.num_actions] = self.action
+        self.obs_history = np.roll(self.obs_history, -self.config.num_obs)
+        self.obs_history[-self.config.num_obs:] = self.obs
+
+        # Convert to PyTorch tensors
+        obs_tensor = torch.from_numpy(self.obs).unsqueeze(0)
+        obs_history_tensor = torch.from_numpy(self.obs_history).unsqueeze(0)
+        cmd_tensor = torch.from_numpy(self.cmd * self.config.cmd_scale * self.config.max_cmd).unsqueeze(0)
+
+        self.latents = self.encoder(obs_history_tensor).detach()
+        self.action = self.policy(torch.cat([obs_tensor, cmd_tensor, self.latents], dim=1)).detach().numpy().squeeze()
+        self.target_dof_pos = self.action * self.config.action_scale + self.config.default_angles
+
+        for i in range(len(self.config.leg_joint2motor_idx)):
+            motor_idx = self.config.leg_joint2motor_idx[i]
+            self.low_cmd.motor_cmd[motor_idx].q = self.target_dof_pos[i]
+            self.low_cmd.motor_cmd[motor_idx].kp = self.config.kps[i]
+            self.low_cmd.motor_cmd[motor_idx].dq = 0.0
+            self.low_cmd.motor_cmd[motor_idx].kd = self.config.kds[i]
+            self.low_cmd.motor_cmd[motor_idx].tau = 0.0
+
+    def Ctrl(self):
+        pass
+        # self.Forward()
+
+    # Send command
+    def SendCmd(self):
+        self.low_cmd.crc = CRC().Crc(self.low_cmd)
+        self.lowcmd_publisher_.Write(self.low_cmd)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        ChannelFactoryInitialize(1, "lo")
+    else:
+        ChannelFactoryInitialize(0, sys.argv[1])
+
+    config_path = f"{LEGGED_GYM_ROOT_DIR}/deploy/real/config/go2.yaml"
+    config = Config(config_path)
+    rl_deploy = RLDeploy(config)
+
+    while True:
+        time.sleep(1)
