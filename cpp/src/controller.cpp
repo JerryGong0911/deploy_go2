d1619432c8789f9c29fd237138ac83755094b317
wzhbaba <1595401605@qq.com>
2026-01-12 20:56:39 +0800

    feat: simplify logging in Forward method by reducing output to history size

--- cpp/src/controller.cpp
+++ cpp/src/controller.cpp
@@ -140,11 +140,7 @@ auto Controller::Forward() {
   std::vector<float> obs_vector = ComputeObs();
   obs_history_buf_.Insert(obs_vector);
   obs_history_ = obs_history_buf_.GetFlattenedData();
-  std::cout << "Obs: ";
-  for (const auto& val : obs_history_) {
-    std::cout << val << " ";
-  }
-  std::cout << std::endl;
+  std::cout << obs_history_.size() << std::endl;
 
   for (int i = 0; i < motor_idx_.size(); ++i) {
     low_cmd->motor_cmd()[motor_idx_[i]].q() =
